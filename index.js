126d06d966ca4ff4cb7f9a340cdd44fb2b885d0c
Akhand Pratap Shukla <akhandshukla36@gmail.com>
2025-09-20 21:17:32 +0530

    CommonJS modules

---
+++ index.js
@@ -0,0 +1,5 @@
+const {generaterandomNumber, celciusToFahrenheit} = require('./utils');
+
+console.log(`Random Number: ${generaterandomNumber()}`);
+
+console.log(`Celcius to  fahrenheit: ${celciusToFahrenheit(0)}`);
---
+++ utils.js
@@ -0,0 +1,13 @@
+function generaterandomNumber(){
+    return Math.floor(Math.random() * 100) +1;
+}
+
+function celciusToFahrenheit(celcius) {
+    return (celcius * 9) / 5 + 32;
+}
+
+
+module.exports = {
+    generaterandomNumber,
+    celciusToFahrenheit,
+};
