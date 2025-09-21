c0974282f7ec51803e6febd2ae5dfa54f3b04a25
Akhand Pratap Shukla <akhandshukla36@gmail.com>
2025-09-21 23:15:05 +0530

    Loading Files

--- server.js
+++ server.js
@@ -1,16 +1,71 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import  path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res)  => {
+
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res)  => {
     //res.setHeader('Content-Type', 'text/html');
     //res.statusCode = 404;
     //res.write('Hello World!');
-    console.log(req.url);
-    console.log(req.method);
+    //console.log(req.url);
+    //console.log(req.method);
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    //res.end(JSON.stringify({ message: 'Server Error' }));
-    res.end('<h1>Hello World!</h1>');
+    try {
+        //Check if GET request
+        if(req.method === 'GET'){
+            let filePath;
+            if (req.url === '/'){
+                filePath = path.join(__dirname, 'public', 'index.html');
+                //res.writeHead(200, { 'Content-Type': 'text/html' });
+                //res.end(JSON.stringify({ message: 'Server Error' }));
+                //res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about'){
+                filePath = path.join(__dirname, 'public', 'about.html');
+                //res.writeHead(200, { 'Content-Type': 'text/html' });
+                //res.end(JSON.stringify({ message: 'Server Error' }));
+                //res.end('<h1>About</h1>');
+            } else {
+                throw new Error('Not Found');
+                //res.writeHead(200, { 'Content-Type': 'text/html' });
+                //res.end(JSON.stringify({ message: 'Server Error' }));
+                //res.end('<h1>Not Found</h1>');
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        }
+        else {
+            throw new Error('Method Not Allowed');
+        }
+    }  catch (error) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            //res.end(JSON.stringify({ message: 'Server Error' }));
+            res.end('Server Error');
+    }
+    /*if (req.url === '/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.end(JSON.stringify({ message: 'Server Error' }));
+        res.end('<h1>Homepage</h1>');
+    } else if (req.url === '/about'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.end(JSON.stringify({ message: 'Server Error' }));
+        res.end('<h1>About</h1>');
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.end(JSON.stringify({ message: 'Server Error' }));
+        res.end('<h1>Not Found</h1>');
+    }*/
+
+    
 });
 
 server.listen(PORT, () => {
